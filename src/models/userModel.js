d3ad37df4e7de9ad4e358d3b101bc7e8cf0034e5
AlexisMadridB <jordimadrid52@gmail.com>
2026-04-26 11:34:09 -0500

    Creación modelo usuarios

---
+++ src/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const usuarioSchema = new mongoose.Schema({
+  nombre: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  rol: {
+    type: String,
+    enum: ["usuario", "admin"],
+    default: "usuario"
+  },
+  fechaRegistro: {
+    type: Date,
+    default: Date.now
+  },
+  perfil: {
+    foto: {
+      type: String,
+      default: ""
+    },
+    descripcion: {
+      type: String,
+      default: ""
+    }
+  }
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
