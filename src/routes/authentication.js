96b3960ff256e8f012dfb9b458bd36e22f9878e0
JulietaLC <julieta.lozanoc@konradlorenz.edu.co>
2026-04-26 13:29:27 -0500

    Aplicar verify token en recipeRoutes.js

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Usuario = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/signup", async (req, res) => {
     try {
@@ -13,52 +14,56 @@ router.post("/signup", async (req, res) => {
             password
         });
 
-        user.password = await user.encryptPassword(user.password);
-
         await user.save();
 
-        // res.json(user);
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.SECRET,
+            { expiresIn: 60 * 60 * 24 }
+        );
+
         res.json({
-            message: "Usuario guardado."
+            auth: true,
+            token
         });
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 });
 
 router.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
+        const user = await Usuario.findOne({ email });
 
-    if (!email || !password) {
-      return res.status(400).json({ error: "Faltan datos" });
+        if (!email || !password) {
+            return res.status(400).json({ error: "Faltan datos" });
+        }
+
+        if (!user) {
+            return res.status(400).json({ error: "Usuario no encontrado" });
+        }
+        const validPassword = await user.comparePassword(password);
+
+        if (!validPassword) {
+            return res.status(400).json({ error: "Contraseña incorrecta" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.SECRET,
+            { expiresIn: 60 * 60 * 24 }
+        );
+
+        res.json({
+            auth: true,
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    const user = await Usuario.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({ error: "Usuario no encontrado" });
-    }
-
-    const validPassword = await bcrypt.compare(password, user.password);
-
-    if (!validPassword) {
-      return res.status(400).json({ error: "Contraseña incorrecta" });
-    }
-
-    res.json({
-      error: null,
-      message: "Bienvenido",
-      user: {
-        id: user._id,
-        nombre: user.nombre,
-        email: user.email
-      }
-    });
-
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
 });
 
 module.exports = router;
